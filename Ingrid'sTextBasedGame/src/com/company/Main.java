6f95659546aed840b84259e20654b6b6c480e199
Doctor 13 <ingridmarquezknirs@gmail.com>
2019-10-22 13:53:18 -0400

    first commit

---
+++ Ingrid'sTextBasedGame/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter username! ");
+        String userName = scan.nextLine();
+        System.out.println("Username is: " + userName);
+    }
+}
